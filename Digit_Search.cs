2f3de94b3e80444316f44ae161b579c32c27e996
Edna1112 <92922936+Edna1112@users.noreply.github.com>
2021-11-04 14:00:59 +0200

    Add files via upload

---
+++ Digit_Search.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace AnythingGoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("put a float number with a .");
+
+            string number = Console.ReadLine();
+            Console.WriteLine("put a digit to search");
+            char digit= Convert.ToChar(Console.ReadLine());
+            string subnumber = number.Substring(number.IndexOf("."));
+             if (subnumber.Contains(digit))
+             {
+                 Console.WriteLine("true");
+             }
+             else
+             {
+                 Console.WriteLine("false");
+             }
+            
+        }
+        
+
+    }
+
+    }
+
